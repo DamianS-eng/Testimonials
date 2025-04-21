dfd2294a327b96c350301dbb14993e722f8e892f
Damian <damiansuaz@gmail.com>
2025-04-21 16:37:19 -0400

    Update index.js

--- docs/index.js
+++ docs/index.js
@@ -23,16 +23,26 @@ const testimonialList = [
 
 const mainEle = document.querySelector('main');
 
+function changeFocus(chosen) {
+}
+
 window.onload = function(e) {
+  const testimonialFocus = document.createElement('p');
+  testimonialFocus.id = 'testimonial-focus';
+  const testimonialRow = document.createElement('div');
+  testimonialRow.id = 'testimonial-row';
+  const nameFocus = document.createElement('p');
+  nameFocus.id = 'testimonial-focus-name';
   testimonialList.forEach((i) => {
-    const testimonialFocus = document.createElement('p');
-    testimonialFocus.id = 'testimonial-focus';
-    const testimonialRow = document.createElement('div');
-    testimonialRow.id = 'testimonial-row';
-    const nameFocus = document.createElement('p');
-    nameFocus.id = 'testimonial-focus-name';
-    mainEle.appendChild(testimonialFocus);
-    mainEle.appendChild(testimonialRow);
-    mainEle.appendChild(nameFocus);
-  });
+    const testimonialIcon = document.createElement('span');
+    testimonialIcon.classList.add('testimonial-icon');
+    const testimonialImg = document.createElement('img');
+    testimonialImg.src = i.img;
+    testimonialImg.alt = i.name;
+    testImonialIcon.appendChild(testimonialImg);
+    testimonialRow.appendChild(testimonialIcon);
+  });  
+  mainEle.appendChild(testimonialFocus);
+  mainEle.appendChild(testimonialRow);
+  mainEle.appendChild(nameFocus);
 }
